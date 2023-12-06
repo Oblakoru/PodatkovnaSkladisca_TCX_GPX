ea46739f054d7d6ad8bafe262131ae16dd1b3ef1
Benjamin <108634496+Oblakoru@users.noreply.github.com>
2023-12-06 11:59:05 +0100

    branje tcx in gpx v CSV

--- main.py
+++ main.py
@@ -4,21 +4,26 @@ import pandas as pd
 from sport_activities_features import GPXFile
 from sport_activities_features.tcx_manipulation import TCXFile
 import tcxparser
+from sqlalchemy import create_engine
 
 
-# Class for reading TCX files
 tcx_file = TCXFile()
 
 data = []
+
+for folder_name in os.listdir("Sport5"):
+    # Join the directory path and folder name to get the full path
+    full_path = os.path.join("Sport5", folder_name)
+    print(full_path)
+
+
 for root, dirs, files in os.walk("Test"):
    for datoteka in files:
         file_path = os.path.join(root, datoteka)
         if os.path.isfile(file_path):
            try:
 
-
                integral_metrics = tcxparser.TCXParser(file_path)
-
 
                tipAktivnosti = integral_metrics.activity_type
                zacetekAktivnosti = integral_metrics.started_at
@@ -47,24 +52,58 @@ for root, dirs, files in os.walk("Test"):
                print(f"Error pri {file_path}")
 
 
+
+
    df = pd.DataFrame(data)
-   print(df)
+
+   df.fillna(0.0, inplace=True)
+   df['kadenceAvgAktivnosti'] = df['kadenceAvgAktivnosti'].astype(float)
+
+   ##
+   df_biking = df[df['tipAktivnosti'] == 'biking']
+
+   kilometrina = ["tipAktivnosti",  "zacetekAktivnosti", "trajanjeAktivnosti", "razdaljaAktivnosti", "tempoAktivnosti" ]
+
+   ostali = [ "kalorijeAktivnosti", "vzponAktivnosti", "kadenceAvgAktivnosti" ]
+
+   df_kilometrina = df_biking[kilometrina]
+   df_ostali = df_biking[ostali]
+
+   # Example for MySQL: 'mysql://username:password@localhost:3306/your_database'
+   povezava = ('mysql+mysqlconnector://root:root@localhost:3308/skladisca_kolesar')
+
+   engine = create_engine(povezava, echo=True)
+
+   df_kilometrina.to_sql('dim_kilometrina', con=engine, if_exists='replace', index=False)
+   df_ostali.to_sql('dim_ostali', con=engine, if_exists='replace', index=False)
+
+   #df.to_csv('test.csv', index=False)
+
+  # print(df)
 
 
 
-### Convert
+### ZA GPX nevem kaj naj bi blo treba naredit?
 
-for root, dirs, files in os.walk("TestGTX"):
-   for datoteka in files:
-        file_path = os.path.join(root, datoteka)
+# for root, dirs, files in os.walk("TestGTX"):
+#    for datoteka in files:
+#         file_path = os.path.join(root, datoteka)
+#
+#         gpx_file = GPXFile()
+#         podatki = gpx_file.read_one_file(file_path)
+#         df = podatki.toDataFrame()
+#
+#         print(df)
 
-        gpx_file = GPXFile()
-        podatki = gpx_file.read_one_file(file_path)
-        for x in podatki:
-            print(x)
-        integral_metrics = gpx_file.extract_integral_metrics(file_path)
 
-        print(integral_metrics)
+
+        # for x in podatki:
+        #     print(x)
+        # integral_metrics = gpx_file.extract_integral_metrics(file_path)
+        #
+        # print(integral_metrics)
+
+
         #data = gpx_file.read_one_file(file_path)
 #
 
